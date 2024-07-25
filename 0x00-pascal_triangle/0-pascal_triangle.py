11c8ce4c2642681842278e02db4c74a021670166
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-07-25 18:23:18 +0100

    Pascal Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+def pascal_triangle(n):
+    """Generates a Pascal Triangle of size n.
+
+    Args:
+    n: The number of rows in the Pascal Triangle.
+
+    Returns:
+    A list of lists representing the Pascal Triangle.
+    """
+
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
