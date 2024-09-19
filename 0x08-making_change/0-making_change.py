ee3162d92eef8113cf81f5e920367487bdf45bd2
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-09-19 22:17:37 +0100

    123

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,32 +3,31 @@
 Module determines fewest number of coins needed to meet a given amount total
 """
 
+from typing import List
 
-def lower_max(arr: list[int]) -> list[int]:
+def lower_max(arr: List[int]) -> List[int]:
     """removes highest number from array"""
-    new_arr = sorted(set(arr))
+    new_arr: List[int] = sorted(set(arr))
     new_arr.pop()
     return new_arr
 
-
-def makeChange(coins: list[int], total: int) -> int:
+def makeChange(coins: List[int], total: int) -> int:
     """
     determines fewest number of coins needed to meet a given amount total
     """
-
     if total <= 0:
         return 0
     if not coins:
         return -1
 
-    highest = max(coins)
+    highest: int = max(coins)
 
     if highest > total:
         coins = lower_max(coins)
         return makeChange(coins, total)
 
-    remaining_total = total - highest
-    result_with_highest = makeChange(coins, remaining_total)
+    remaining_total: int = total - highest
+    result_with_highest: int = makeChange(coins, remaining_total)
 
     if result_with_highest != -1:
         return 1 + result_with_highest
