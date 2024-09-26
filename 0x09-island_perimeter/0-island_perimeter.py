63801aa749c6522d74f48677d5ffefd3cb55894c
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-09-26 19:37:46 +0100

    Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+module creates a function returns the perimeter of island described in grid
+"""
+from typing import List
+
+
+def island_perimeter(grid: List[List[int]]) -> int:
+    """function returns the perimeter of the island described in grid"""
+    perimeter = 0
+    rows = len(grid)
+    columns = len(grid[0])
+
+    for i in range(rows):
+        for j in range(columns):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i-1][j] == 0:
+                    perimeter += 1
+                if i == rows - 1 or grid[i+1][j] == 0:
+                    perimeter += 1
+                if j == 0 or grid[i][j-1] == 0:
+                    perimeter += 1
+                if j == columns - 1 or grid[i][j+1] == 0:
+                    perimeter += 1
+    return perimeter
